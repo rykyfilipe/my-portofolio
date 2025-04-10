8d2077ab88322146a9ddbec5b64bb6eee7fa1a90
rykyfilipe <161625737+rykyfilipe@users.noreply.github.com>
2025-04-10 12:28:44 +0300

    updates to responsive content

--- src/Components/CustumCursor.jsx
+++ src/Components/CustumCursor.jsx
@@ -1,89 +1,89 @@
 /** @format */
 
-import { useState, useEffect } from "react";
+import {useEffect, useState} from "react";
 import "./CustomCursor.css";
 
 const CustomCursor = () => {
-	const [circlePosition, setCirclePosition] = useState({ x: 0, y: 0 });
-	const [dotPosition, setDotPosition] = useState({ x: 0, y: 0 });
-	const [isMoving, setIsMoving] = useState(false);
-	const [lastMoveTime, setLastMoveTime] = useState(0);
+    const [circlePosition, setCirclePosition] = useState({x: 0, y: 0});
+    const [dotPosition, setDotPosition] = useState({x: 0, y: 0});
+    const [isMoving, setIsMoving] = useState(false);
+    const [lastMoveTime, setLastMoveTime] = useState(0);
 
-	useEffect(() => {
-		let animationFrameId = null;
-		let timeout = null;
+    useEffect(() => {
+        let animationFrameId = null;
+        let timeout = null;
 
-		const updateCursorPosition = (e) => {
-			const currentTime = Date.now();
-			setIsMoving(true);
-			setLastMoveTime(currentTime);
+        const updateCursorPosition = (e) => {
+            const currentTime = Date.now();
+            setIsMoving(true);
+            setLastMoveTime(currentTime);
 
-			if (timeout) clearTimeout(timeout);
-			timeout = setTimeout(() => {
-				setIsMoving(false);
-			}, 200);
+            if (timeout) clearTimeout(timeout);
+            timeout = setTimeout(() => {
+                setIsMoving(false);
+            }, 200);
 
-			setDotPosition((prevPos) => {
-				const dx = e.clientX - prevPos.x;
-				const dy = e.clientY - prevPos.y;
-				return {
-					x: prevPos.x + dx * 0.2,
-					y: prevPos.y + dy * 0.2,
-				};
-			});
-		};
+            setDotPosition((prevPos) => {
+                const dx = e.clientX - prevPos.x;
+                const dy = e.clientY - prevPos.y;
+                return {
+                    x: prevPos.x + dx * 0.2,
+                    y: prevPos.y + dy * 0.2,
+                };
+            });
+        };
 
-		const animateCircle = () => {
-			setCirclePosition((prevPos) => {
-				const dx = dotPosition.x - prevPos.x;
-				const dy = dotPosition.y - prevPos.y;
-				return {
-					x: prevPos.x + dx * 0.05,
-					y: prevPos.y + dy * 0.05,
-				};
-			});
+        const animateCircle = () => {
+            setCirclePosition((prevPos) => {
+                const dx = dotPosition.x - prevPos.x;
+                const dy = dotPosition.y - prevPos.y;
+                return {
+                    x: prevPos.x + dx * 0.05,
+                    y: prevPos.y + dy * 0.05,
+                };
+            });
 
-			animationFrameId = requestAnimationFrame(animateCircle);
-		};
+            animationFrameId = requestAnimationFrame(animateCircle);
+        };
 
-		window.addEventListener("mousemove", updateCursorPosition);
-		animationFrameId = requestAnimationFrame(animateCircle);
+        window.addEventListener("mousemove", updateCursorPosition);
+        animationFrameId = requestAnimationFrame(animateCircle);
 
-		return () => {
-			window.removeEventListener("mousemove", updateCursorPosition);
-			if (animationFrameId) cancelAnimationFrame(animationFrameId);
-			if (timeout) clearTimeout(timeout);
-		};
-	}, [dotPosition]);
+        return () => {
+            window.removeEventListener("mousemove", updateCursorPosition);
+            if (animationFrameId) cancelAnimationFrame(animationFrameId);
+            if (timeout) clearTimeout(timeout);
+        };
+    }, [dotPosition]);
 
-	const getDotStyle = () => {
-		if (!isMoving) {
-			return {
-				left: `${circlePosition.x}px`,
-				top: `${circlePosition.y}px`,
-				transition: "left 0.3s ease-out, top 0.3s ease-out",
-			};
-		}
+    const getDotStyle = () => {
+        if (!isMoving) {
+            return {
+                left: `${circlePosition.x}px`,
+                top: `${circlePosition.y}px`,
+                transition: "left 0.3s ease-out, top 0.3s ease-out",
+            };
+        }
 
-		return {
-			left: `${dotPosition.x}px`,
-			top: `${dotPosition.y}px`,
-			transition: "none",
-		};
-	};
+        return {
+            left: `${dotPosition.x}px`,
+            top: `${dotPosition.y}px`,
+            transition: "none",
+        };
+    };
 
-	return (
-		<>
-			<div
-				className='cursor-circle'
-				style={{
-					left: `${circlePosition.x}px`,
-					top: `${circlePosition.y}px`,
-				}}
-			/>
-			<div className='cursor-dot' style={getDotStyle()} />
-		</>
-	);
+    return (
+        <>
+            <div
+                className='cursor-circle hidden md:block '
+                style={{
+                    left: `${circlePosition.x}px`,
+                    top: `${circlePosition.y}px`,
+                }}
+            />
+            <div className='cursor-dot hidden md:block' style={getDotStyle()}/>
+        </>
+    );
 };
 
 export default CustomCursor;
--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -7,40 +7,41 @@ import github from "../assets/icons/github.svg";
 import googleplay from "../assets/icons/googleplay.svg";
 
 function Hero() {
-	return (
-		<section className='w-screen h-screen gradient-radial flex flex-col md:flex-row'>
-			<div className='w-full md:w-[50%] h-full flex flex-col justify-center items-start pl-10 md:ml-20'>
-				<h1 className='absolute top-10 left-10 text-3xl text-white'>
-					! Under development
-				</h1>
+    return (
+        <section className='w-screen h-screen gradient-radial flex flex-col md:flex-row'>
+            <div
+                className='w-full md:w-[50%] h-full flex flex-col justify-center items-start pl-5 md:pl-10 md:ml-20'>
+                <h1 className='absolute top-10 left-10 text-3xl text-white'>
+                    ! Under development
+                </h1>
 
-				<h1 className='text-white text-3xl md:text-7xl font-bienale font-bold w-max leading-25'>
-					👋I'm RYKY
-				</h1>
-				<h1
-					className='text-3xl md:text-7xl md:w-max font-bienale font-bold  leading-[1.2]
+                <h1 className='text-white text-3xl md:text-7xl font-bienale font-bold w-max leading-25'>
+                    👋I'm RYKY
+                </h1>
+                <h1
+                    className='text-3xl md:text-7xl md:w-max font-bienale font-bold  leading-[1.2]
 								text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-white to-blue-500 
 								bg-[length:200%_200%] animate-[gradient_3s_ease-in-out_infinite]'>
-					Web Design +<br /> ReactNative Developer
-				</h1>
+                    Web Design +<br/> ReactNative Developer
+                </h1>
 
-				<div className='mt-10 leading-5 text-white text-md font-bienale font-normal   '>
-					<p>Welcome to my digital playground!</p>
-					<p>
-						Let's embark on a journey of creativity and innovation together.
-					</p>
-				</div>
+                <div className='mt-10 leading-5 text-white text-md font-bienale font-normal   '>
+                    <p>Welcome to my digital playground!</p>
+                    <p>
+                        Let's embark on a journey of creativity and innovation together.
+                    </p>
+                </div>
 
-				<div className='flex gap-4 mt-10'>
-					<Button text='Contact Me' imgURL={chat} />
-					<Button imgURL={linkedin} />
-					<Button imgURL={github} />
-					<Button imgURL={googleplay} />
-				</div>
-			</div>
-			<div className='hidden md:block w-[50%] h-full  justify-center items-center '></div>
-		</section>
-	);
+                <div className='flex gap-4 mt-10'>
+                    <Button text='Contact Me' imgURL={chat}/>
+                    <Button imgURL={linkedin}/>
+                    <Button imgURL={github}/>
+                    <Button imgURL={googleplay}/>
+                </div>
+            </div>
+            <div className='hidden md:block w-[50%] h-full  justify-center items-center '></div>
+        </section>
+    );
 }
 
 export default Hero;
